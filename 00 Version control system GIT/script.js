00f682d4bd9bbe2fc2cc29991ba5d018f3461b9d
sergeySchmidt <draggsbony@gmail.com>
2022-06-20 23:08:51 +0300

    20/06/2022

---
+++ 00 Version control system GIT/script.js
@@ -0,0 +1,8 @@
+//terminal.integrated.cwd                                                     git push -u origin maingit push -u origin mainкоманда для изменения папки в терминале 
+//git status                                                                  проверка отслеживаемых и неотслеживаемых файлов
+//git init                                                                    инициализация гит репозитория
+//git add -A                                                                  добавление всех файлов в гит репозиторий
+//git add script.js                                                           добавление отдельного файла в гит репозиторий 
+//git commit -a -m"20.06.2022"                                                создание контрольной точки (сохранения) в репозитории
+//git log                                                                     просмотр логов (истории) изменения репозитория 
+//git remote add origin https://github.com/Draggsbonys/Cheat-sheets-JS.git    связывание локального репозитория с удаленным 
